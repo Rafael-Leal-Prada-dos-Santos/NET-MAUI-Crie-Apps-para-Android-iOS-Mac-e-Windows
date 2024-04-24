fc46b80d63a5437791e90e27b03f0a2ae1676eec
Rafael Santos <rpsantos@iportsolutions.com.br>
2024-04-24 13:52:01 -0300

    Até aula 10

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SIGPLUSLib;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static SigPlus sig = new SigPlus();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void InitTablet()
+        {
+            sig.InitSigPlus();
+        }
+
+        
+    }
+}
